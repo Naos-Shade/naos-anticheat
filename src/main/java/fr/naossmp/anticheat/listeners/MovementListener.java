67033688757af6066996a4f96fd991f1e52a5381
Naos-Shade <jegouerwan@gmail.com>
2026-05-23 16:43:38 +0200

    Remove Speed/Fly/KillAura checks, keep XRay + death tracking

--- src/main/java/fr/naossmp/anticheat/listeners/MovementListener.java
+++ src/main/java/fr/naossmp/anticheat/listeners/MovementListener.java
@@ -1,25 +1,5 @@
 package fr.naossmp.anticheat.listeners;
 
-import fr.naossmp.anticheat.NaosAnticheat;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
 
-public class MovementListener implements Listener {
-
-    private final NaosAnticheat plugin;
-
-    public MovementListener(NaosAnticheat plugin) {
-        this.plugin = plugin;
-    }
-
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onMove(PlayerMoveEvent event) {
-        if (!event.hasChangedPosition()) return;
-
-        var player = event.getPlayer();
-        plugin.getSpeedCheck().check(player, event.getFrom(), event.getTo());
-        plugin.getFlyCheck().check(player, event.getTo());
-    }
-}
+public class MovementListener implements Listener {}
